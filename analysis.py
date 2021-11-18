956f40ed5c1c570b3fb7ee8349a8b0b1dd06d5b5
TheKeron <xkeron19@gmail.com>
2021-11-18 15:28:11 +0100

    Done

--- analysis.py
+++ analysis.py
@@ -49,20 +49,27 @@ fault = {
 }
 
 weather = {
-    1 : "Nestížené",
-    2 : "Mlha",
-    3 : "Na počátku deště",
-    4 : "Déšť",
-    5 : "Sněžení",
-    6 : "Náledí",
-    7 : "Nárazový vítr"
+    1: "Nestížené",
+    2: "Mlha",
+    3: "Na počátku deště",
+    4: "Déšť",
+    5: "Sněžení",
+    6: "Náledí",
+    7: "Nárazový vítr"
 }
 
 
-"""
-Get 
-"""
 def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
+    """
+    Get dataframe from file and convert types to category with some exeptions
+
+    Arguments:
+        filename - name of file with data
+        verbose - print size of data in MB before and after type conversion
+
+    Return:
+        Function return converted dataframe
+    """
     df = pd.read_pickle(filename)
     if verbose:
         print(f"orig_size={df.memory_usage(deep=True).sum() / 1048576:.1f} MB")
@@ -83,7 +90,16 @@ def get_dataframe(filename: str, verbose: bool = False) -> pd.DataFrame:
 # Ukol 2: počty nehod v jednotlivých regionech podle druhu silnic
 def plot_roadtype(df: pd.DataFrame, fig_location: str = None,
                   show_figure: bool = False):
-    df_type = df[df['region'].isin(['JHC', 'JHM', 'HKK', 'KVK'])][['region', 'p21']]
+    """
+    Plot number of accidents on different road types
+
+    Arguments:
+        df - dataframe with data
+        fig_location - if specified, store graph in given location
+        show_figure - show figure if True
+    """
+    df_type = df[df['region'].isin(
+                ['JHC', 'JHM', 'HKK', 'KVK'])][['region', 'p21']]
     df_type['tmp'] = 1
     df_type.loc[df_type['p21'] == 4] = 3
     df_type = df_type.groupby(['region', 'p21']).agg('sum').reset_index()
@@ -118,19 +134,31 @@ def plot_roadtype(df: pd.DataFrame, fig_location: str = None,
 # Ukol3: zavinění zvěří
 def plot_animals(df: pd.DataFrame, fig_location: str = None,
                  show_figure: bool = False):
+    """
+    Plot number of accidents including wildlife or domestic animals
 
+    Arguments:
+        df - dataframe with data
+        fig_location - if specified, store graph in given location
+        show_figure - show figure if True
+    """
     df_animals = df[df['p58'] == 5]
-    df_animals = df_animals[df_animals['region'].isin(['JHC', 'JHM', 'HKK', 'KVK'])][['region', 'p10', 'date']]
-    df_animals = df_animals[(df_animals['date'] >= pd.to_datetime('2016-1-1')) & (df_animals['date'] <= pd.to_datetime('2020-12-31'))]
+    df_animals = df_animals[df_animals['region'].isin(
+                    ['JHC', 'JHM', 'HKK', 'KVK'])][['region', 'p10', 'date']]
+    df_animals = df_animals[
+                (df_animals['date'] >= pd.to_datetime('2016-1-1'))
+                & (df_animals['date'] <= pd.to_datetime('2020-12-31'))]
+
     df_animals['p10'] = df_animals['p10'].map(fault)
     df_animals['date'] = df_animals['date'].dt.month
     df_animals['tmp'] = 1
-    df_animals = df_animals.groupby(['region', 'date', 'p10']).agg('sum').reset_index()
+    df_animals = df_animals.groupby(
+                ['region', 'date', 'p10']).agg('sum').reset_index()
 
     sns.set_palette(["#52006A", "#FF7600", "#CD113B"])
     g = sns.catplot(x="date", y='tmp', data=df_animals, col="region",
                     kind="bar", col_wrap=2, height=3, aspect=1.5,
-                    ci=None, hue="p10", sharex=False, sharey=True)
+                    ci=None, hue="p10", sharex=False, sharey=False)
     (g.set_titles("Kraj: {col_name}")
         .set_axis_labels("Měsíc", "Počet nehod")
         .fig.suptitle('Zavinění nehod', weight="bold"))
@@ -147,21 +175,34 @@ def plot_animals(df: pd.DataFrame, fig_location: str = None,
 # Ukol 4: Povětrnostní podmínky
 def plot_conditions(df: pd.DataFrame, fig_location: str = None,
                     show_figure: bool = False):
-    
-    df_w = df[df['region'].isin(['JHC', 'JHM', 'HKK', 'KVK'])][['region', 'p18', 'date']]
-    df_w = df_w[(df_w['date'] >= pd.to_datetime('2016-1-1')) & (df_w['date'] <= pd.to_datetime('2020-12-31'))]
+    """
+    Plot number of accidents in different weather conditions
+
+    Arguments:
+        df - dataframe with data
+        fig_location - if specified, store graph in given location
+        show_figure - show figure if True
+    """
+    df_w = df[df['region'].isin(
+            ['JHC', 'JHM', 'HKK', 'KVK'])][['region', 'p18', 'date']]
+    df_w = df_w[
+            (df_w['date'] >= pd.to_datetime('2016-1-1'))
+            & (df_w['date'] <= pd.to_datetime('2020-12-31'))]
+
     df_w = df_w[df_w['p18'] != 0]
     df_w['p18'] = df_w['p18'].map(weather)
     df_w['tmp'] = 1
-    df_w = df_w.pivot_table(columns='p18', values='tmp', index=['date', 'region'], aggfunc='sum').reset_index()
+    df_w = df_w.pivot_table(columns='p18', values='tmp', aggfunc='sum',
+                            index=['date', 'region']).reset_index()
     df_w = df_w.fillna(0)
-    df_w = df_w.groupby([pd.Grouper(key='date', freq='M'), 'region']).agg('sum').reset_index()
-    df_w = df_w.melt(id_vars=["region", 'date'], var_name="p18", value_name="value")
-    
+    df_w = df_w.groupby([pd.Grouper(key='date', freq='M'),
+                        'region']).agg('sum').reset_index()
+    df_w = df_w.melt(id_vars=["region", 'date'],
+                     var_name="p18", value_name="value")
 
     sns.set_palette("bright")
     g = sns.relplot(data=df_w, kind="line", x="date", y='value', hue='p18',
-                    col='region', col_wrap=2, height=3, aspect=1.5, 
+                    col='region', col_wrap=2, height=3, aspect=1.5,
                     facet_kws={'sharey': False, 'sharex': False})
 
     (g.set_titles("Kraj: {col_name}")
@@ -172,7 +213,7 @@ def plot_conditions(df: pd.DataFrame, fig_location: str = None,
     date_form = DateFormatter("%m/%y")
     for ax in g.axes.flatten():
         ax.xaxis.set_major_formatter(date_form)
-    
+
     g.tight_layout()
     if fig_location:
         plt.savefig(fig_location)
